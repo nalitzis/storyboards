2619996deffde15c418d233bbaa8a531c6e37fd8
Ado <ado@adoMacBook.local>
2012-03-21 21:12:17 +0100

    Initial Commit

---
+++ TestSingleView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSingleView
+//
+//  Created by Ado on 21/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestSingleView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestSingleView
+//
+//  Created by Ado on 21/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
